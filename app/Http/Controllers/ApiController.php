1b425201246bb0f3e7c4efa693295768d431969e
Aditya Ardiansyah <03adityaardiansyah1999@gmail.com>
2021-06-27 09:05:53 +0700

    update api

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -30,37 +30,49 @@ class ApiController extends Controller
 
     public function get_nwnp(Request $request)
     {
-        $periode = $request->periode;
-        $data = [
-            'employee_id' => '',
-            'date' => '',
-            'flag_attendance' => '',
-            'description' => ''
-        ];
-        $tidak_hadir = [];
-
-        // tarik data pegawai
-        $pegawai = Employee::select('id', 'salary', 'allowance')->where('active', 'ON')->get();
-        foreach ($pegawai as $key => $value) {
-            //looping dengan bulan 
-            for ($i = 1; $i <= date('t', strtotime($periode)); $i++) {
-                $absen = Attendance::select('id')->where('employee_id', $value->id)
-                    ->where('date', $periode . '-' . $i)
-                    ->first();
-                // jika tidak absen
-                if (empty($absen)) {
-                    // status tidak hadir
-                    if (date('N', strtotime($periode . '-' . $i)) < 6) {
-                        $data['employee_id'] = $value->id;
-                        $data['date'] = $periode . '-' . $i;
-                        $data['description'] = 'Tidak Hadir';
-                        $data['flag_attendance'] = 0;
-                        array_push($tidak_hadir, $data);
-                    } 
+        try {
+            $periode = $request->periode;
+            $data = [
+                'employee_id' => '',
+                'date' => '',
+                'flag_attendance' => '',
+                'description' => ''
+            ];
+            $tidak_hadir = [];
+    
+            // tarik data pegawai
+            $pegawai = Employee::select('id', 'salary', 'allowance')->where('active', 'ON')->get();
+            foreach ($pegawai as $key => $value) {
+                //looping dengan bulan 
+                for ($i = 1; $i <= date('t', strtotime($periode)); $i++) {
+                    $absen = Attendance::select('id')->where('employee_id', $value->id)
+                        ->where('date', $periode . '-' . $i)
+                        ->first();
+                    // jika tidak absen
+                    if (empty($absen)) {
+                        // status tidak hadir
+                        if (date('N', strtotime($periode . '-' . $i)) < 6) {
+                            $data['employee_id'] = $value->id;
+                            $data['date'] = $periode . '-' . $i;
+                            $data['description'] = 'Tidak Hadir';
+                            $data['flag_attendance'] = 0;
+                            array_push($tidak_hadir, $data);
+                        } 
+                    }
                 }
+            
             }
-        
+            return response()->json([
+                'error' => false,
+                'message' => 'Data berhasil ditemukan',
+                'data' => $tidak_hadir
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'error' => false,
+                'message' => $th->getMessage(),
+                'data' => []
+            ]);
         }
-        return $tidak_hadir;
     }
 }
